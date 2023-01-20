f02efc93e0c4843ff6f1606e2e8a029baf1c10ec
Ariel Wiznia <ariel@lean-labs.com>
2023-01-19 22:12:32 -0300

    Fuck you, Elon

--- content.js
+++ content.js
@@ -1,3 +1,9 @@
-setTimeout(function() {
+const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount));
+
+async function load() {
+  await wait(500);
   document.querySelector('[role="tablist"] [role="presentation"]:last-child a').click();
-}, 500);
+}
+
+load();
+
