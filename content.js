333bf93518c21c82d76ad6d2f6fab1c88a479943
Ariel Wiznia <wiznia@gmail.com>
2023-01-20 17:27:48 -0300

    Update content.js

--- content.js
+++ content.js
@@ -2,7 +2,9 @@ const wait = (amount = 0) => new Promise(resolve => setTimeout(resolve, amount))
 
 async function load() {
   await wait(500);
-  document.querySelector('[role="tablist"] [role="presentation"]:last-child a').click();
+  if (document.querySelector('[role="tablist"] [role="presentation"]:last-child a')) {
+    document.querySelector('[role="tablist"] [role="presentation"]:last-child a').click();
+  }
 }
 
 load();
